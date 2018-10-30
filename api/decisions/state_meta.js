27ab9e46d3cbe7c06ea7f29d9bca9061b6ca1fba
Dmitry Panov <dpanov76@yahoo.com>
2018-10-30 17:03:49 +0000

    Working dynamic form builder. Still some elements has to be finished

--- api/decisions/state_meta.js
+++ api/decisions/state_meta.js
@@ -44,155 +44,148 @@ module.exports = {
             header: 'What should appear in the directions order?',
             groups: [
                 {
-                    fieldset: {
-                        legend: {
-                            text: 'Reasons the consent order was not approved',
-                            isPageHeading: true,
-                            classes: 'govuk-fieldset__legend--m'
+                    fieldset: [
+                        {
+                            legend: {
+                                text: 'Reasons the consent order was not approved',
+                                isPageHeading: true,
+                                classes: 'govuk-fieldset__legend--m'
+                            }
                         },
-                        hint: { text: 'Select all that apply.' },
-                        checkbox: {
-                            control: 'partiesNeedAttend',
-                            value: false,
-                            text: 'The parties need to attend a hearing'
+                        {
+                            hint: { text: 'Select all that apply.' }
                         },
-                        checkbox: {
-                            control: 'partiesNeedAttend',
-                            value: false,
-                            text: 'The parties need to attend a hearing',
-                            groups: [
-                                {
-                                    control: 'partiesNeedAttend1',
-                                    value: false,
-                                    text: 'The parties need to attend a hearing1'
-                                }
-                            ]
-                        },
-                        radios: {
-                            control: 'includeAnnotatedVersionDraftConsOrder',
-                            radioGroup: [
-                                {
-                                    value: 'yes',
-                                    text: 'Yes',
-                                    hiddenAccessibilityText: ', send an annotated version of the draft consent order to the parties'
-                                },
-                                {
-                                    value: 'no',
-                                    text: 'No',
-                                    hiddenAccessibilityText:
-                                        ', I don’t want to send an annotated version of the draft consent order to the parties'
-                                }
-                            ]
-                        },
-                        textarea: {
-                            label: {
-                                text: 'Directions',
-                                classes: 'govuk-label--m'
-                            },
-                            control: 'Directions',
-                            value: 'Direction text'
-                        },
-                        checkboxes: [
-                            {
+                        {
+                            checkbox: {
                                 control: 'partiesNeedAttend',
                                 value: false,
                                 text: 'The parties need to attend a hearing'
-                            },
-                            {
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'NotEnoughInformation',
                                 value: false,
                                 text: 'Not enough information was supplied to decide if the order is fair',
-                                sub: {
-                                    fieldset: {
-                                        legend: {
-                                            text: 'Information required',
-                                            isPageHeading: false,
-                                            classes: 'govuk-fieldset__legend--m'
-                                        },
-                                        checkboxes: [
+                                groups: [
+                                    {
+                                        fieldset: [
                                             {
-                                                control: 'capitalPositions',
-                                                value: false,
-                                                text: 'The parties’ capital positions if the order were to take effect'
+                                                legend: {
+                                                    text: 'Information required',
+                                                    isPageHeading: false,
+                                                    classes: 'govuk-fieldset__legend--m'
+                                                }
                                             },
                                             {
-                                                control: 'partiesHousingNeeds',
-                                                value: false,
-                                                text: 'The parties’ housing needs and whether they are met by the order'
+                                                checkbox: {
+                                                    control: 'capitalPositions',
+                                                    value: false,
+                                                    text: 'The parties’ capital positions if the order were to take effect'
+                                                }
                                             },
                                             {
-                                                control: 'justificationDeparture',
-                                                value: false,
-                                                text: 'The justification for departure from equality of capital'
+                                                checkbox: {
+                                                    control: 'partiesHousingNeeds',
+                                                    value: false,
+                                                    text: 'The parties’ housing needs and whether they are met by the order'
+                                                }
                                             },
                                             {
-                                                control: 'partiesPensionProvision',
-                                                value: false,
-                                                text: 'The parties’ pension provision if the order were to take effect'
+                                                checkbox: {
+                                                    control: 'justificationDeparture',
+                                                    value: false,
+                                                    text: 'The justification for departure from equality of capital'
+                                                }
                                             },
                                             {
-                                                control: 'childrensHousingNeeds',
-                                                value: false,
-                                                text: 'The children’s housing needs and whether they are met by the order'
+                                                checkbox: {
+                                                    control: 'partiesPensionProvision',
+                                                    value: false,
+                                                    text: 'The parties’ pension provision if the order were to take effect'
+                                                }
                                             },
                                             {
-                                                control: 'netEffectOrder',
-                                                value: false,
-                                                text: 'The net effect of the order'
+                                                checkbox: {
+                                                    control: 'childrensHousingNeeds',
+                                                    value: false,
+                                                    text: 'The children’s housing needs and whether they are met by the order'
+                                                }
                                             },
                                             {
-                                                control: 'Other',
-                                                value: false,
-                                                text: 'Other',
-                                                sub: {
-                                                    textareas: [
+                                                checkbox: {
+                                                    control: 'netEffectOrder',
+                                                    value: false,
+                                                    text: 'The net effect of the order'
+                                                }
+                                            },
+                                            {
+                                                checkbox: {
+                                                    control: 'Other',
+                                                    value: false,
+                                                    text: 'Other',
+                                                    groups: [
                                                         {
-                                                            label: {
-                                                                text: 'What information is needed?',
-                                                                classes: 'govuk-label--m'
-                                                            },
-                                                            control: 'informationNeeded',
-                                                            value: 'Information text'
+                                                            textarea: {
+                                                                label: {
+                                                                    text: 'What information is needed?',
+                                                                    classes: 'govuk-label--m'
+                                                                },
+                                                                control: 'informationNeeded',
+                                                                value: 'Information text'
+                                                            }
                                                         }
                                                     ]
                                                 }
                                             }
                                         ]
                                     }
-                                }
-                            },
-                            {
+                                ]
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'orderNotAppearOfS25ca1973',
                                 value: false,
                                 text: 'The order doesn’t appear fair taking account of S25 Matrimonial Causes Act 1973'
-                            },
-                            {
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'd81',
                                 value: false,
                                 text: 'The D81 form is incomplete'
-                            },
-                            {
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'pensionAnnex',
                                 value: false,
                                 text: 'The pension annex was not attached'
-                            },
-                            {
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'applicantTakenAdvice',
                                 value: false,
                                 text: 'It’s not clear if the applicant has taken independent legal advice'
-                            },
-                            {
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'respondentTakenAdvice',
                                 value: false,
                                 text: 'It’s not clear if the respondent has taken independent legal advice'
-                            },
-                            {
+                            }
+                        },
+                        {
+                            checkbox: {
                                 control: 'Other2',
                                 value: false,
                                 text: 'Other',
-                                sub: {
-                                    textareas: [
-                                        {
+                                groups: [
+                                    {
+                                        textarea: {
                                             label: {
                                                 text: 'Reason',
                                                 classes: 'govuk-label--m'
@@ -200,11 +193,39 @@ module.exports = {
                                             control: 'Reason',
                                             value: 'Reason text'
                                         }
-                                    ]
-                                }
+                                    }
+                                ]
                             }
-                        ]
-                    }
+                        },
+                        {
+                            radios: {
+                                control: 'includeAnnotatedVersionDraftConsOrder',
+                                radioGroup: [
+                                    {
+                                        value: 'yes',
+                                        text: 'Yes',
+                                        hiddenAccessibilityText: ', send an annotated version of the draft consent order to the parties'
+                                    },
+                                    {
+                                        value: 'no',
+                                        text: 'No',
+                                        hiddenAccessibilityText:
+                                            ', I don’t want to send an annotated version of the draft consent order to the parties'
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            textarea: {
+                                label: {
+                                    text: 'Directions',
+                                    classes: 'govuk-label--m'
+                                },
+                                control: 'Directions',
+                                value: 'Direction text'
+                            }
+                        }
+                    ]
                 },
                 {
                     textarea: {
@@ -215,33 +236,33 @@ module.exports = {
                         control: 'Directions',
                         value: 'Direction text'
                     }
-                },
-                {
-                    fieldset: {
-                        legend: {
-                            text: 'Do you want to include an annotated version of the draft consent order?',
-                            isPageHeading: false,
-                            classes: 'govuk-fieldset__legend--m'
-                        },
-                        hint: { text: 'You can use this to illustrate any detailed points or feedback for the parties.' },
-                        radios: {
-                            control: 'includeAnnotatedVersionDraftConsOrder',
-                            radioGroup: [
-                                {
-                                    value: 'yes',
-                                    text: 'Yes',
-                                    hiddenAccessibilityText: ', send an annotated version of the draft consent order to the parties'
-                                },
-                                {
-                                    value: 'no',
-                                    text: 'No',
-                                    hiddenAccessibilityText:
-                                        ', I don’t want to send an annotated version of the draft consent order to the parties'
-                                }
-                            ]
-                        }
-                    }
                 }
+                // {
+                //     fieldset: {
+                //         legend: {
+                //             text: 'Do you want to include an annotated version of the draft consent order?',
+                //             isPageHeading: false,
+                //             classes: 'govuk-fieldset__legend--m'
+                //         },
+                //         hint: { text: 'You can use this to illustrate any detailed points or feedback for the parties.' },
+                //         radios: {
+                //             control: 'includeAnnotatedVersionDraftConsOrder',
+                //             radioGroup: [
+                //                 {
+                //                     value: 'yes',
+                //                     text: 'Yes',
+                //                     hiddenAccessibilityText: ', send an annotated version of the draft consent order to the parties'
+                //                 },
+                //                 {
+                //                     value: 'no',
+                //                     text: 'No',
+                //                     hiddenAccessibilityText:
+                //                         ', I don’t want to send an annotated version of the draft consent order to the parties'
+                //                 }
+                //             ]
+                //         }
+                //     }
+                // }
             ],
             buttons: [
                 {
