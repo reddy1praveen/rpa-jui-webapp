29c38b450980f7ce1b4fd28b5803c37aef71c52d
Alan McCabe-Jones <alan.work@teamsmog.com>
2018-11-22 20:13:12 +0000

    initial scss state

--- api/controllers/decisions/scss/templates/index.js
+++ api/controllers/decisions/scss/templates/index.js
@@ -1,11 +1,10 @@
-
-export const templates = {}
+const templates = {}
 
 // Define all the case type template includes, right now benefit
-
 templates.benefit = {}
-
 // end case type definition
 
 // Define all the pages for a case type
-templates.benefit.create = require('benefit-create')
+templates.benefit.create = require('./benefit/create')
+
+module.exports = templates
