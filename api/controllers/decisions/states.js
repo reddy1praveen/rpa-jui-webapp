ce95181e159bf5adc42daea61649067d52e408b0
alan <alan.work@teamsmog.com>
2018-11-23 13:07:14 +0000

    typo

--- api/controllers/decisions/states.js
+++ api/controllers/decisions/states.js
@@ -7,47 +7,46 @@ const sscs = 'SSCS'
 
 const mapping = require('./divorce/mapping.js')
 
-// // does not handle OR yet
-// function handleCondition(condition, i) {
-//     // index 0 hardcoded, not interating through for OR
-//     const key = Object.keys(condition.condition[0])[0]
-//     if (variables[key] === condition.condition[key]) {
-//         console.log('found')
-//     }
+// does not handle OR yet
+function handleCondition(condition, i) {
+    // index 0 hardcoded, not interating through for OR
+    const key = Object.keys(condition.condition[0])[0]
+    if (variables[key] === condition.condition[key]) {
+        console.log('found')
+    }
+}
 
-// }
+function handleState(state) {
+    if (state.condition) {
+        handleCondition(state.conditon)
+    } else if (state.conditions) {
+        state.conditions.some(handleCondition)
+    }
+}
 
-// function handleState(state) {
-//     if (state.condition) {
-//         handleCondition(state.conditon)
-//     } else if (state.conditions) {
-//         state.conditions.some(handleCondition)
-//     }
-// }
-
-// function process(event, state) {
-//     mapping.some((instruction, i) => {
-//         if (instruction.event === event) {
-//             // event is the main index and so there can only be one instruction per event - exit after finding
-//             if (instruction.state) {
-//                 handleState(instruction.state)
-//             } else if (instruction.states) {
-//                 instruction.states.array.forEach(state => {
-//                     handleState(state)
-//                 })
-//             }
-//             return true
-//         }
-//         return false
-//     })
-// }
+function process(event, state) {
+    mapping.some((instruction, i) => {
+        if (instruction.event === event) {
+            // event is the main index and so there can only be one instruction per event - exit after finding
+            if (instruction.state) {
+                handleState(instruction.state)
+            } else if (instruction.states) {
+                instruction.states.array.forEach(state => {
+                    handleState(state)
+                })
+            }
+            return true
+        }
+        return false
+    })
+}
 
 function handleStateRoute(req, res) {
     const jurisdiction = req.params.jurId
 
     switch (jurisdiction) {
         case divorce:
-            divorceCscssallback(req, res)
+            divorceCallback(req, res)
             break
         case sscs:
             scssCallback(req, res)
