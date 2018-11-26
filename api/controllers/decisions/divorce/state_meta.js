e43beb61c816435e17a4185281fff0ec3b0e7b61
Dmitry Panov <dpanov76@yahoo.com>
2018-11-26 12:13:13 +0000

    Masoods form changes taken off from FR state meta  to examples file

--- api/controllers/decisions/divorce/state_meta.js
+++ api/controllers/decisions/divorce/state_meta.js
@@ -17,87 +17,12 @@ module.exports = {
                         {
                             radios: {
                                 control: 'approveDraftConsent',
+                                classes: 'govuk-radios--inline',
                                 radioGroup: [
                                     {
                                         value: 'yes',
                                         text: 'Yes',
-                                        hiddenAccessibilityText: 'some hidden text',
-                                        groups: [
-                                          {
-                                              legend: {
-                                                  text: 'Choose a color',
-                                                  isPageHeading: true,
-                                                  classes: 'govuk-fieldset__legend--m'
-                                              }
-                                          },
-                                          {
-                                            radios: {
-                                                control: 'favoriteColor',
-                                                radioGroup: [
-                                                    {
-                                                        value: 'black',
-                                                        text: 'Black',
-                                                        hiddenAccessibilityText: 'some hidden text',
-                                                        groups: [
-                                                          {
-                                                              legend: {
-                                                                  text: 'Please confirm your choice',
-                                                                  isPageHeading: true,
-                                                                  classes: 'govuk-fieldset__legend--m'
-                                                              }
-                                                          },
-                                                          {
-                                                            radios: {
-                                                                control: 'pleaseConfirm',
-                                                                radioGroup: [
-                                                                    {
-                                                                        value: 'yes',
-                                                                        text: 'Yes',
-                                                                        hiddenAccessibilityText: 'some hidden text'
-                                                                    },
-                                                                    {
-                                                                        value: 'no',
-                                                                        text: 'No',
-                                                                        hiddenAccessibilityText: 'some hidden text'
-                                                                    }
-                                                                ]
-                                                              }
-                                                          }
-                                                        ]
-                                                    },
-                                                    {
-                                                        value: 'white',
-                                                        text: 'White',
-                                                        hiddenAccessibilityText: 'some hidden text',
-                                                        groups: [
-                                                          {
-                                                              legend: {
-                                                                  text: 'This is Legend element',
-                                                                  isPageHeading: true,
-                                                                  classes: 'govuk-fieldset__legend--m'
-                                                              }
-                                                          },
-                                                          {
-                                                              input: {
-                                                                  label: {
-                                                                      text: 'Estimate length of hearing in minutes',
-                                                                      classes: 'govuk-label--m'
-                                                                  },
-                                                                  control: 'estimateLengthOfHearing',
-                                                                  classes: 'govuk-input--width-3'
-                                                              }
-                                                          }
-                                                        ]
-                                                    },
-                                                    {
-                                                        value: 'red',
-                                                        text: 'Red',
-                                                        hiddenAccessibilityText: 'some hidden text'
-                                                    }
-                                                ]
-                                              }
-                                          }
-                                        ]
+                                        hiddenAccessibilityText: 'some hidden text'
                                     },
                                     {
                                         value: 'no',
@@ -371,6 +296,7 @@ module.exports = {
                         {
                             radios: {
                                 control: 'includeAnnotatedVersionDraftConsOrder',
+                                classes: 'govuk-radios--inline',
                                 validationError: {
                                     value: 'Select yes if you want to include an annotated version of the draft consent order',
                                     controlId: 'includeAnnotatedVersionDraftConsOrder'
