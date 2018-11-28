92da4338e2fc3315b29d18590945ed666348e28a
Dmitry Panov <dpanov76@yahoo.com>
2018-11-28 12:29:47 +0000

    set-award-dates page is done

--- api/controllers/decisions/sscs/templates/benefit/finalDecision.js
+++ api/controllers/decisions/sscs/templates/benefit/finalDecision.js
@@ -1,129 +1,14 @@
 module.exports = {
-    idPrefix: 'create',
-    name: 'create',
-    header: 'Do you want to approve the draft consent order?',
+    idPrefix: 'final-decision',
+    name: 'final-decision',
+    header: 'Make final decision',
     groups: [
         {
-            fieldset: [
-                {
-                    radios: {
-                        control: 'approveDraftConsent',
-                        radioGroup: [
-                            {
-                                value: 'yes',
-                                text: 'Yes',
-                                hiddenAccessibilityText: 'some hidden text',
-                                groups: [
-                                  {
-                                      legend: {
-                                          text: 'Choose a color',
-                                          isPageHeading: true,
-                                          classes: 'govuk-fieldset__legend--m'
-                                      }
-                                  },
-                                  {
-                                    radios: {
-                                        control: 'favoriteColor',
-                                        radioGroup: [
-                                            {
-                                                value: 'black',
-                                                text: 'Black',
-                                                hiddenAccessibilityText: 'some hidden text',
-                                                groups: [
-                                                  {
-                                                      legend: {
-                                                          text: 'Please confirm your choice',
-                                                          isPageHeading: true,
-                                                          classes: 'govuk-fieldset__legend--m'
-                                                      }
-                                                  },
-                                                  {
-                                                    radios: {
-                                                        control: 'pleaseConfirm',
-                                                        radioGroup: [
-                                                            {
-                                                                value: 'yes',
-                                                                text: 'Yes',
-                                                                hiddenAccessibilityText: 'some hidden text'
-                                                            },
-                                                            {
-                                                                value: 'no',
-                                                                text: 'No',
-                                                                hiddenAccessibilityText: 'some hidden text'
-                                                            }
-                                                        ]
-                                                      }
-                                                  }
-                                                ]
-                                            },
-                                            {
-                                                value: 'white',
-                                                text: 'White',
-                                                hiddenAccessibilityText: 'some hidden text',
-                                                groups: [
-                                                  {
-                                                      legend: {
-                                                          text: 'This is Legend element',
-                                                          isPageHeading: true,
-                                                          classes: 'govuk-fieldset__legend--m'
-                                                      }
-                                                  },
-                                                  {
-                                                    date: {
-                                                            formName: 'form1',
-                                                            day: {
-                                                                  input: {
-                                                                      label: {
-                                                                          text: 'Day',
-                                                                          classes: 'govuk-date-input__label'
-                                                                      },
-                                                                      control: 'awardEndDateDay',
-                                                                      classes: 'govuk-date-input__input govuk-input--width-2'
-                                                                  }
-                                                            },
-                                                            month: {
-                                                                  input: {
-                                                                      label: {
-                                                                          text: 'Month',
-                                                                          classes: 'govuk-date-input__label'
-                                                                      },
-                                                                      control: 'awardEndDateMonth',
-                                                                      classes: 'govuk-date-input__input govuk-input--width-2'
-                                                                  }
-                                                            },
-                                                            year:{
-                                                                  input: {
-                                                                      label: {
-                                                                          text: 'Year',
-                                                                          classes: 'govuk-date-input__label'
-                                                                      },
-                                                                      control: 'awardEndDateYear',
-                                                                      classes: 'govuk-date-input__input govuk-input--width-4'
-                                                                  }
-                                                      }
-                                                    }
-                                                  },
-                                                ]
-                                            },
-                                            {
-                                                value: 'red',
-                                                text: 'Red',
-                                                hiddenAccessibilityText: 'some hidden text'
-                                            }
-                                        ]
-                                      }
-                                  }
-                                ]
-                            },
-                            {
-                                value: 'no',
-                                text: 'No',
-                                hiddenAccessibilityText: 'some hidden text'
-                            }
-                        ]
-                    }
-                }
-            ]
+            textarea: {
+                label: 'Enter decision notes',
+                control: 'decisionNotes',
+                value: ''
+            }
         },
         {
             button: {
--- api/controllers/decisions/sscs/templates/benefit/setAwardDates.js
+++ api/controllers/decisions/sscs/templates/benefit/setAwardDates.js
@@ -1,2 +1,135 @@
 module.exports = {
+    idPrefix: 'create',
+    name: 'create',
+    header: 'Do you want to approve the draft consent order?',
+    groups: [
+        {
+            legend: {
+                text: 'Start date',
+                isPageHeading: true,
+                classes: 'govuk-fieldset__legend--m'
+            }
+        },
+        {
+            hint: {
+                text: 'For example, 12 4 2018',
+                classes: 'govuk-hint'
+            }
+        },
+        {
+            date: {
+                    formName: 'startDate',
+                    day: {
+                          input: {
+                              label: {
+                                  text: 'Day',
+                                  classes: 'govuk-date-input__label'
+                              },
+                              control: 'awardEndDateDay',
+                              classes: 'govuk-date-input__input govuk-input--width-2'
+                          }
+                    },
+                    month: {
+                          input: {
+                              label: {
+                                  text: 'Month',
+                                  classes: 'govuk-date-input__label'
+                              },
+                              control: 'awardEndDateMonth',
+                              classes: 'govuk-date-input__input govuk-input--width-2'
+                          }
+                    },
+                    year:{
+                          input: {
+                              label: {
+                                  text: 'Year',
+                                  classes: 'govuk-date-input__label'
+                              },
+                              control: 'awardEndDateYear',
+                              classes: 'govuk-date-input__input govuk-input--width-4'
+                          }
+                    }
+              }
+        },
+        {
+            fieldset: [
+                {
+                    radios: {
+                        control: 'approveDraftConsent',
+                        radioGroup: [
+                            {
+                                value: 'endDate',
+                                text: 'Set end date',
+                                hiddenAccessibilityText: 'some hidden text',
+                                groups: [
+                                  {
+                                      legend: {
+                                          text: 'End date',
+                                          isPageHeading: true,
+                                          classes: 'govuk-fieldset__legend--m'
+                                      }
+                                  },
+                                  {
+                                      hint: {
+                                          text: 'For example, 12 4 2019',
+                                          classes: 'govuk-hint'
+                                      }
+                                  },
+                                  {
+                                    date: {
+                                            formName: 'endDate',
+                                            day: {
+                                                  input: {
+                                                      label: {
+                                                          text: 'Day',
+                                                          classes: 'govuk-date-input__label'
+                                                      },
+                                                      control: 'awardEndDateDay',
+                                                      classes: 'govuk-date-input__input govuk-input--width-2'
+                                                  }
+                                            },
+                                            month: {
+                                                  input: {
+                                                      label: {
+                                                          text: 'Month',
+                                                          classes: 'govuk-date-input__label'
+                                                      },
+                                                      control: 'awardEndDateMonth',
+                                                      classes: 'govuk-date-input__input govuk-input--width-2'
+                                                  }
+                                            },
+                                            year:{
+                                                  input: {
+                                                      label: {
+                                                          text: 'Year',
+                                                          classes: 'govuk-date-input__label'
+                                                      },
+                                                      control: 'awardEndDateYear',
+                                                      classes: 'govuk-date-input__input govuk-input--width-4'
+                                                  }
+                                      }
+                                    }
+                                  }
+                                ]
+                            },
+                            {
+                                value: 'indefinite',
+                                text: 'Indefinite award',
+                                hiddenAccessibilityText: 'some hidden text'
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        {
+            button: {
+                control: 'createButton',
+                value: 'Continue',
+                type: 'submit',
+                classes: '',
+                onEvent: 'continue'
+            }
+        }
+    ]
 }
