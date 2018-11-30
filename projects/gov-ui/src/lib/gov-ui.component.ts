d7e049ae7747ce35b44ac577cee3608850339c89
Chris Tarasovs <chris@frontr.co.uk>
2018-11-30 13:28:21 +0000

    Introducing basic setup of Project/UI

---
+++ projects/gov-ui/src/lib/gov-ui.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-gov-ui',
+  template: `
+    <p>
+      gov-ui works!
+    </p>
+  `,
+  styles: []
+})
+export class GovUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/gov-ui/src/lib/gov-ui.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { GovUiComponent } from './gov-ui.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [GovUiComponent],
+  exports: [GovUiComponent]
+})
+export class GovUiModule { }
