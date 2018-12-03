460080f4e4d409eb891482d454621e28c768f68f
ficklephil <philip.aberneithy@gmail.com>
2018-12-03 11:42:52 +0000

    prepare case for approval

--- api/lib/utilities/ccdStoreTokenUtilities.js
+++ api/lib/utilities/ccdStoreTokenUtilities.js
@@ -16,10 +16,12 @@ function getOptions(req) {
 
 async function getTokenAndMakePayload(req, caseId) {
 
+    const authHeaders = getOptions(req)
+
     console.log('getTokenAndMakePayload')
 
+    const user = req.session.user
     const userId = req.auth.userId
-    const authHeaders = getOptions(req)
 
     const jurisdiction = 'DIVORCE'
     const caseType = 'FinancialRemedyMVP2'
@@ -30,9 +32,22 @@ async function getTokenAndMakePayload(req, caseId) {
     const eventToken = eventTokenAndCase.token
     const caseDetails = eventTokenAndCase.caseDetails
 
-    console.log('eventTokenAndCase')
-    console.log(eventToken)
-    console.log(caseDetails)
+    const directionComments = ''
+    // console.log(eventToken);
+    // console.log(caseDetails);
+
+    const payload = prepareCaseForApproval(
+        eventToken,
+        eventId,
+        user,
+        directionComments
+    )
+
+    console.log('payload')
+    console.log(payload)
+
+    // console.log(eventToken)
+    // console.log(caseDetails)
 }
 
 /**
@@ -40,6 +55,7 @@ async function getTokenAndMakePayload(req, caseId) {
  *
  * TODO: Not sure if we need both Authorization and ServiceAuthorization on this request.
  * TODO: Error handling passing back meaningful error.
+ * TODO: Unit Test
  *
  * @param {String} userId - '96842'
  * @param {String} caseId - '1540909451019845'
@@ -69,35 +85,50 @@ async function getEventTokenAndCase(userId, caseId, jurisdiction, caseType, even
     }
 }
 
-// if (decision === 'yes') {
-//     payload = perpareCaseForApproval(
-//         caseDetails,
-//         eventToken,
-//         'FR_approveApplication',
-//         req.session.user,
-//         store.get(`decisions_${state.inCaseId}`)
-//     )
-// }
+/**
+ * perpareCaseForApproval
+ *
+ * TODO: Make DRY with state.js
+ * TODO: Unit Test
+ * TODO: directionComments used to be store.get(`decisions_${state.inCaseId} store.notesForAdmin
+ *
+ * @param eventToken
+ * @param {String} eventId - 'FR_approveApplication'
+ * @param {String} directionComments - the comments that go along with the
+ *
+ * @return {Object} -
+ * {
+ *  data:
+ *  {
+ *  orderDirection: 'Order Accepted as drafted',
+ *  orderDirectionDate: '2018-12-03',
+ *  orderDirectionJudge: 'District Judge',
+ *  orderDirectionJudgeName: 'Nasim Judge '
+ *  },
+ *  event: { id: 'FR_approveApplication' },
+ *  event_token: 'eyJhbG...',
+ *  ignore_warning: true
+ * }
+ */
+function prepareCaseForApproval(eventToken, eventId, user, directionComments) {
+    return {
+        /* eslint-disable-next-line id-blacklist */
+        data: {
+            orderDirection: 'Order Accepted as drafted',
+            orderDirectionDate: moment(new Date()).format('YYYY-MM-DD'),
+            orderDirectionJudge: 'District Judge',
+            orderDirectionJudgeName: `${user.forename} ${user.surname} `,
+            orderDirectionAddComments: directionComments
+        },
+        event: {
+            id: eventId
+        },
+        event_token: eventToken,
 
-// function perpareCaseForApproval(caseData, eventToken, eventId, user, store) {
-//
-//     return {
-//         /* eslint-disable-next-line id-blacklist */
-//         data: {
-//             orderDirection: 'Order Accepted as drafted',
-//             orderDirectionDate: moment(new Date()).format('YYYY-MM-DD'),
-//             orderDirectionJudge: 'District Judge',
-//             orderDirectionJudgeName: `${user.forename} ${user.surname} `,
-//             orderDirectionAddComments: store.notesForAdmin
-//         },
-//         event: {
-//             id: eventId
-//         },
-//         event_token: eventToken,
-//
-//         ignore_warning: true
-//     }
-// }
+        ignore_warning: true
+    }
+}
 
 module.exports.getTokenAndMakePayload = getTokenAndMakePayload
 module.exports.getEventTokenAndCase = getEventTokenAndCase
+module.exports.prepareCaseForApproval = prepareCaseForApproval
