4aa77fd8298b30c238cf52637dcb258e38a96eef
Chris Tarasovs <chris@frontr.co.uk>
2018-12-04 15:42:04 +0000

    Feat: Introducing custom ui module

---
+++ projects/gov-ui-custom/src/lib/gov-ui-custom.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-gov-ui-custom',
+  template: `
+    <p>
+      gov-ui-custom works!
+    </p>
+  `,
+  styles: []
+})
+export class GovUiCustomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
