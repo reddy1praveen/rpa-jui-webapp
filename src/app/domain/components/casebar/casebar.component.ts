9c7c41bf8bef5e724d09bb5dd0e6a6a69dda2ab8
fazanki <fazanki@gmail.com>
2018-12-13 13:12:16 +0000

    moving the case actions to casebaer

--- src/app/domain/components/casebar/casebar.component.ts
+++ src/app/domain/components/casebar/casebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import 'rxjs/add/operator/filter';
 import {CaseDataOther} from '../../../routing/pages/modules/case';
+import {LinkItem} from '../../case-viewer/components/summary-panel/models/elements.module';
 
 @Component({
         selector: 'app-casebar',
@@ -10,7 +11,8 @@ import {CaseDataOther} from '../../../routing/pages/modules/case';
 export class CaseBarComponent {
 
     @Input() case: CaseDataOther;
-
+    public createLink: LinkItem = { href: '../decision/create', text: 'Make decision' };
+    public hearingLink: LinkItem = {href: '../hearing/list', text: 'List for hearing'};
     constructor() {}
 
 }
