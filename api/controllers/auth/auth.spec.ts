0598f5ab8687c1dbcdc843738c376ec1cc89c3d7
Alan Jones <alan.work@teamsmog.com>
2018-12-18 17:33:42 +0000

    refactor of auth functionality

--- api/controllers/auth/auth.spec.ts
+++ api/controllers/auth/auth.spec.ts
@@ -2,11 +2,14 @@ import 'mocha'
 
 import * as chai from 'chai'
 import { expect } from 'chai'
-import * as simonChai from 'sinon-chai'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
 import { mockReq, mockRes } from 'sinon-express-mock'
-chai.use(simonChai)
+
+chai.use(sinonChai)
 
 import { config } from '../../../config'
+import * as idam from '../../services/idam-api/idam-api'
 import { logout } from './index'
 
 describe('Auth', () => {
@@ -25,4 +28,40 @@ describe('Auth', () => {
             expect(res.redirect).to.be.calledWith('/')
         })
     })
+
+
+
+    describe('auth', () => {
+        let req
+        let res
+        beforeEach(() => {
+            const idamSpy = sinon.spy(idam, 'postOauthToken')
+
+            sinon.stub(idam, 'postOauthToken').callsFake(function fakeFn() {
+             console.log('this has been called 1')
+             return Promise.resolve({access_token: 'access'})
+         })
+            sinon.stub(idam, 'getDetails').callsFake(function fakeFn() {
+             console.log('this has been called')
+             return Promise.resolve({id: 1, name: 'testuser'})
+         })
+ 
+            req = mockReq({
+                get: () => 'localhost',
+                query: {
+                    code: 1,
+                },
+            })
+            res = mockRes()
+        })
+
+        it('should set the authorisation header', async () => {
+            await authenticateUser(req, res, null)
+           // expect(idam.postOauthToken).to.be.calledWith(1, 'localhost')
+            // expect(idam.getDetails).to.have.been.calledWith(sinon.match({ headers: { Authorization: 'Bearer access' } }))
+            expect(idam.getDetails).to.have.been.called()
+        })
+    })
+
+
 })
--- api/controllers/auth/index.ts
+++ api/controllers/auth/index.ts
@@ -1,5 +1,7 @@
 import * as express from 'express'
+import * as log4js from 'log4js'
 import { config } from '../../../config/index'
+import { asyncReturnOrError, exists } from '../../lib/util'
 
 const { getDetails, postOauthToken } = require('../../services/idam-api/idam-api')
 
@@ -8,35 +10,52 @@ const cookieUserId = config.cookies.userId
 
 const test = true
 
+const logger = log4js.getLogger('auth')
+logger.level = config.logging || 'off'
+
 export function logout(req, res) {
     res.clearCookie(cookieToken)
     res.clearCookie(cookieUserId)
     res.redirect(req.query.redirect || '/')
 }
 
+export async function authenticateUser(req: any, res) {
+
+    const data = await asyncReturnOrError(
+        postOauthToken(req.query.code, req.get('host')),
+        'Error getting token for code',
+        res,
+        logger,
+        false
+        )
+
+    if (exists(data, 'access_token')) {
+        const options = { headers: { Authorization: `Bearer ${data.access_token}` } }
+
+        const details = await asyncReturnOrError(
+            getDetails(options),
+            'Cannot get user details',
+            res,
+            logger,
+            false
+        )
+
+        if (details) {
+            req.session.user = details
+            res.cookie(cookieToken, data.access_token)
+            res.cookie(cookieUserId, details.id)
+        }
+    }
+
+    res.redirect('/')
+ }
+
 export function auth(app) {
     const router = express.Router()
 
     app.use('/oauth2/callback', router)
 
-    router.use((req: any, res, next) => {
-        postOauthToken(req.query.code, req.get('host'))
-            .then(data => {
-                if (data.access_token) {
-                    const options = { headers: { Authorization: `Bearer ${data.access_token}` } }
-                    getDetails(options).then(details => {
-                        req.session.user = details
-                        res.cookie(cookieToken, data.access_token)
-                        res.cookie(cookieUserId, details.id)
-                        res.redirect('/')
-                    })
-                }
-            })
-            .catch(e => {
-                console.log('error - ', e)
-                res.redirect('/')
-            })
-    })
+    router.use(authenticateUser)
 
     app.use('/logout', logout)
 }
