b921305a537dfd968409172c4ef013a7099bae90
Dmitry Panov <dpanov76@yahoo.com>
2019-01-04 17:26:01 +0000

    RIUI-738 done

--- api/controllers/decisions/divorce/templates/divorce/create.ts
+++ api/controllers/decisions/divorce/templates/divorce/create.ts
@@ -1,13 +1,73 @@
+// module.exports = {
+//     idPrefix: 'create',
+//     name: 'create',
+//     header: 'Make a decision',
+//     formGroupValidators: [],
+//     validationHeaderErrorMessages: [
+//         {
+//             validationLevel: 'formControl',
+//             controlId: 'makeDecision',
+//             text: 'Select yes if you want to make a decision',
+//             href: '#'
+//         }
+//     ],
+//     groups: [
+//         {
+//             fieldset: [
+//                 {
+//                     legend: {
+//                         text: 'Are you satisfied that petitioner is entitled to a decree of divorce on the ground that the marriage has broken down irretrievably?',
+//                         isPageHeading: true,
+//                         classes: 'govuk-fieldset__legend--m'
+//                     }
+//                 },
+//                 {
+//                     radios: {
+//                         control: 'makeDecision',
+//                         classes: 'govuk-radios--inline',
+//                         validators: ['required'],
+//                         validationError: {
+//                             value: 'Please select one of the option',
+//                             controlId: 'makeDecision'
+//                         },
+//                         radioGroup: [
+//                             {
+//                                 value: 'yes',
+//                                 text: 'Yes',
+//                                 hiddenAccessibilityText: 'some hidden text'
+//                             },
+//                             {
+//                                 value: 'no',
+//                                 text: 'No',
+//                                 hiddenAccessibilityText: 'some hidden text'
+//                             }
+//                         ]
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             button: {
+//                 control: 'createButton',
+//                 value: 'Continue',
+//                 type: 'submit',
+//                 classes: '',
+//                 onEvent: 'continue'
+//             },
+//         },
+//     ],
+// }
+
 module.exports = {
-    idPrefix: 'create',
-    name: 'create',
-    header: 'Make a decision',
+    idPrefix: 'costs-order',
+    name: 'costs-order',
+    header: 'What type of costs order is needed?',
     formGroupValidators: [],
     validationHeaderErrorMessages: [
         {
             validationLevel: 'formControl',
-            controlId: 'makeDecision',
-            text: 'Select yes if you want to make a decision',
+            controlId: 'costOrder',
+            text: 'Please choose an order',
             href: '#'
         }
     ],
@@ -15,31 +75,117 @@ module.exports = {
         {
             fieldset: [
                 {
-                    legend: {
-                        text: 'Are you satisfied that petitioner is entitled to a decree of divorce on the ground that the marriage has broken down irretrievably?',
-                        isPageHeading: true,
-                        classes: 'govuk-fieldset__legend--m'
-                    }
-                },
-                {
                     radios: {
-                        control: 'makeDecision',
-                        classes: 'govuk-radios--inline',
+                        control: 'costOrder',
+                        classes: '',
                         validators: ['required'],
                         validationError: {
                             value: 'Please select one of the option',
-                            controlId: 'makeDecision'
+                            controlId: 'costOrder'
                         },
                         radioGroup: [
                             {
-                                value: 'yes',
-                                text: 'Yes',
-                                hiddenAccessibilityText: 'some hidden text'
+                                value: 'orderCosts',
+                                text: 'An order that respondent will pay the petitioner’s costs',
+                                hiddenAccessibilityText: 'An order that respondent will pay the petitioner’s costs',
+                                groups: [
+                                    {
+                                        radios: {
+                                            control: 'orderCosts',
+                                            radioGroup: [
+                                                {
+                                                    value: 'agreedTo',
+                                                    text: 'Agreed to',
+                                                    hiddenAccessibilityText: 'Agreed to',
+                                                    groups: [
+                                                        {
+                                                            input: {
+                                                                label: {
+                                                                    text: 'In pounds',
+                                                                    classes: ''
+                                                                },
+                                                                control: 'agreedInPounds',
+                                                                classes: 'govuk-input--width-10 govuk-!-margin-bottom-6'
+                                                            }
+                                                        },
+                                                        {
+                                                            input: {
+                                                                label: {
+                                                                    text: 'Or as a percentage',
+                                                                    classes: ''
+                                                                },
+                                                                control: 'agreedPercentage',
+                                                                classes: 'govuk-input--width-10'
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    value: 'limitedTo',
+                                                    text: 'Limited to',
+                                                    hiddenAccessibilityText: 'Limited to',
+                                                    groups: [
+                                                        {
+                                                            input: {
+                                                                label: {
+                                                                    text: 'In pounds',
+                                                                    classes: ''
+                                                                },
+                                                                control: 'limitedInPounds',
+                                                                classes: 'govuk-input--width-10 govuk-!-margin-bottom-6'
+                                                            }
+                                                        },
+                                                        {
+                                                            input: {
+                                                                label: {
+                                                                    text: 'Or as a percentage',
+                                                                    classes: ''
+                                                                },
+                                                                control: 'limitedPercentage',
+                                                                classes: 'govuk-input--width-10'
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    value: 'ifNotAgreed',
+                                                    text: 'To be subject to detailed assessment if not agreed',
+                                                    hiddenAccessibilityText: 'To be subject to detailed assessment if not agreed'
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ]
                             },
                             {
-                                value: 'no',
-                                text: 'No',
-                                hiddenAccessibilityText: 'some hidden text'
+                                value: 'other',
+                                text: 'Other',
+                                hiddenAccessibilityText: 'Other',
+                                groups: [
+                                    {
+                                        textarea: {
+                                            label: {
+                                                text: 'Details',
+                                                classes:''
+                                            },
+                                            validationError: {
+                                                value: 'Please enter details',
+                                                controlId: 'details'
+                                            },
+                                            control: 'details',
+                                            value: '',
+                                            validators: ['required']
+                                        }
+                                    },
+                                ]
+                            },
+                            {
+                                devider: 'or'
+                            },
+                            {
+                                value: 'noOrder',
+                                text: 'No order for costs',
+                                hiddenAccessibilityText: 'No order for costs'
                             }
                         ]
                     }
